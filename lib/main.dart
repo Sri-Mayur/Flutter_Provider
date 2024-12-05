cf7d6ac96dec4b47acc24bd4690e264cfb6b971f
Mayur Srivastav <mayursrivastava877@gmail.com>
2024-12-05 20:30:30 +0530

    Changed to multiprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:learning_provider/Stateful.dart';
+import 'package:learning_provider/provider/slider_provider.dart';
 import 'package:learning_provider/provider/count_provider.dart';
+import 'package:learning_provider/screen/slider.dart';
 import 'package:learning_provider/screen/count.dart';
 import 'package:provider/provider.dart';
 
@@ -11,18 +12,25 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-        create: (_) => CountProvider(),
-        child: MaterialApp(
-          title: 'Flutter Demo',
-          theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-            useMaterial3: true,
-          ),
-          home: CountExample(),
-        ));
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => SliderProvider()),
+        ChangeNotifierProvider(create: (_) => CountProvider()),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const SliderExample(), // Default screen
+        routes: {
+          '/slider': (context) => const SliderExample(),
+          '/count': (context) => const CountExample(),
+        },
+      ),
+    );
   }
 }
